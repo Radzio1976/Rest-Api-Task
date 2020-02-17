11428935ae467adb6262dcec874798bbdb68a268
Radosław Tomaszewski <r.tomaszewski@gmail.com>
2020-02-17 16:00:43 +0100

    Added logic to send user data on json-server

--- src/App.js
+++ src/App.js
@@ -32,6 +32,76 @@ class App extends React.Component {
     })
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let isValid = true;
+    if (this.state.name.value.length < 5) {
+      this.setState({
+        name: {
+          ...this.state.name,
+          error: "Pole imię musi zawierać conajmniej 5 znaków"
+        }
+      })
+      isValid = false
+    }
+    if (this.state.surname.value.length < 5) {
+      this.setState({
+        surname: {
+          ...this.state.surname,
+          error: "Pole nazwisko musi zawierać conajmniej 5 znaków"
+        }
+      })
+      isValid = false
+    }
+    if (this.state.email.value.includes("@") === false) {
+      this.setState({
+        email: {
+          ...this.state.email,
+          error: "Pole email musi zawierać znak @"
+        }
+      })
+      isValid = false
+    }
+    if (this.state.phone.value.length !== 9) {
+      this.setState({
+        phone: {
+          ...this.state.phone,
+          error: "Pole telefon musi mieć 9 cyfr bez odstępów"
+        }
+      })
+      isValid = false
+    }
+    if (isValid) {
+      Axios.post("http://localhost:3000/users", { name: this.state.name.value, surname: this.state.surname.value, email: this.state.email.value, phone: this.state.phone.value }).then((response) => {
+        Axios.get("http://localhost:3000/users").then((response) => {
+          this.setState({
+            users: response.data,
+            name: {
+              value: "",
+              error: ""
+            },
+            surname: {
+              value: "",
+              error: ""
+            },
+            email: {
+              value: "",
+              error: ""
+            },
+            phone: {
+              value: "",
+              error: ""
+            }
+          })
+        }).catch((error) => {
+          console.log(error)
+        })
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  }
+
   render() {
     return (
       <div className="form">
